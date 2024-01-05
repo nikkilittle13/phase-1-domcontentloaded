6289676335629af121a144bdbdf45be0d6f5d86a
Nikki Little <nikkilittle13@gmail.com>
2024-01-05 12:37:45 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pageText = document.getElementById('text');
+    pageText.textContent = "This is really cool!";
+});
+
